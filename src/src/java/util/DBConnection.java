2406bcab08d08a9f664f345caefc423e3fb2ef6c
ademtarhan <ademtarhan.dev@gmail.com>
2022-05-29 14:47:15 +0300

    pages navigation completed

--- src/src/java/util/DBConnection.java
+++ src/src/java/util/DBConnection.java
@@ -31,18 +31,5 @@ public abstract class DBConnection {
         
         return c;
     }
-    /*public Connection connect() {
-        Connection c = null;
-            try {
-                Class.forName("org.postgresql.Driver");
-                 c = DriverManager
-                        .getConnection("jdbc:postgresql://localhost:5432/news",
-                                "postgres", "12345");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            return c;     
-        
-    }*/
     
 }
